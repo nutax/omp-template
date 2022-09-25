a0b1282cfa03ad80437d2628b7ecea6194bc3905
Jose Ignacio Huby Ochoa <40769942+nutax@users.noreply.github.com>
2022-09-25 21:40:44 +0000

    Change example

--- src/main.c
+++ src/main.c
@@ -14,8 +14,8 @@ int main(int argc, char **argv) {
     NANOS(et_auto, float x = dot(a, b, SIZE));
     NANOS(et_omp, float y = dot_simd(a, b, SIZE));
 
-    printf("[ Auto simd: %ld ns ]  vs  [ Forced simd: %ld ns ]\n", et_auto, et_omp);
-    printf("[ Auto simd: %f ]  vs  [ Forced simd: %f ]\n", x, y);
+    printf("[ Auto simd: %ld ns ]  vs  [ Forced simd: %ld ns ] \n", et_auto, et_omp);
+    printf("[ Auto simd: %f ]  vs  [ Forced simd: %f ] \n", x, y);
   }
 
   return EXIT_SUCCESS;
